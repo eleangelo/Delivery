395f0f247bc646fbb992f62d01f5ea589f8281c3
Elena Angelova <eleangelo0717@gmail.com>
2020-04-10 18:31:00 +0700

    Добавили - при клике на корзину появляется модальное окно, при нажатии на х скрывается. при помощи js. Добавили анимацию.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
+/*1-й вариант*/
+/*cartButton.addEventListener('click', function (event) {
+    modal.classList.add('is-open');
+});
+close.addEventListener('click', function (event) {
+    modal.classList.remove('is-open');
+});*/
+
+/*2-й вариант*/
+cartButton.addEventListener('click', toggleModal);
+close.addEventListener('click', toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle('is-open')
+}
+new WOW().init();
