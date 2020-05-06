4b3a6c7ce816c012e68a16b43090d2e6fbf4bc34
Elena Angelova <eleangelo0717@gmail.com>
2020-05-06 14:14:21 +0700

    Реализовали авторизацию

--- js/main.js
+++ js/main.js
@@ -21,4 +21,70 @@ new WOW().init();
 
 // 1 day
 
-const buttonAuth = document.querySelector('button-auth');
+const buttonAuth = document.querySelector('.button-auth');
+    modalAuth = document.querySelector('.modal-auth');
+    closeAuth = document.querySelector('.close-auth');
+    logInForm = document.querySelector('#logInForm');
+    loginInput = document.querySelector('#login');
+    userName = document.querySelector('.user-name');
+    buttonOut = document.querySelector('.button-out');
+
+    let login = localStorage.getItem('gloDelivery');
+
+function toggleModalAuth() {
+    modalAuth.classList.toggle('is-open');
+}
+
+function authorized() {
+
+    function logOut() {
+        login = null;
+        localStorage.removeItem('gloDelivery');
+        buttonAuth.style.display = '';
+        userName.style.display = '';
+        buttonOut.style.display = '';
+        buttonOut.removeEventListener('click', logOut);
+        checkAuth(); 
+    }
+
+    console.log('Авторизован');
+
+    userName.textContent = login;
+
+    buttonAuth.style.display = 'none';
+    userName.style.display = 'inline';
+    buttonOut.style.display = 'block';
+
+    buttonOut.addEventListener('click', logOut);
+}
+
+function notAuthorized() {
+    console.log('Не авторизован');
+
+    function logIn(event) {
+        event.preventDefault();
+        login = loginInput.value;
+
+        localStorage.setItem('gloDelivery', login);    
+
+        toggleModalAuth();
+        buttonAuth.removeEventListener('click', toggleModalAuth);
+        closeAuth.removeEventListener('click', toggleModalAuth);
+        logInForm.removeEventListener('submit', logIn);
+        logInForm.reset();
+        checkAuth();  
+    }
+
+    buttonAuth.addEventListener('click', toggleModalAuth);
+    closeAuth.addEventListener('click', toggleModalAuth);
+    logInForm.addEventListener('submit', logIn);
+}
+
+function checkAuth() {
+    if (login) {
+        authorized();
+    } else {
+        notAuthorized();
+    }
+}
+checkAuth();
